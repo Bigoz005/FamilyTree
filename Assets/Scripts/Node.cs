e8fe489ca1c2b9f40d668f3a339dcb13021fc8e2
MichałNawrot <michal.nawrot.mn.mn@gmail.com>
2020-09-27 17:17:51 +0200

    Added panel

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    string name;
+    string surname;
+    string date;
+    string spouse;
+    string ex_spouse;
+
+    List<Node> parents;
+    List<Node> siblings;
+    List<Node> kids;
+}
